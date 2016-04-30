ee7a8d6ea18450fc6631c8d8ecf37ea4582df96a
reindels <reindels@oregonstate.edu>
2016-04-30 16:02:46 -0700

    Added Poll ListView in MainActivity  onClick sends an intent to PollSetupActivity, passes poll name.

--- Project1/app/src/main/java/casco/project1/MainActivity.java
+++ Project1/app/src/main/java/casco/project1/MainActivity.java
@@ -1,5 +1,6 @@
 package casco.project1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,15 +9,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +32,13 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
+        list = (ListView) findViewById(R.id.lv_polls);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this,R.array.test_polls, android.R.layout.simple_list_item_1);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(this);
     }
 
     @Override
@@ -48,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent= new Intent(this, PollSetupActivity.class);
+        String[] pollName = getResources().getStringArray(R.array.test_polls);
+        //
+        intent.putExtra("PollName", pollName[position]);
+        startActivity(intent);
     }
 }
--- Project1/app/src/main/java/casco/project1/PollSetupActivity.java
+++ Project1/app/src/main/java/casco/project1/PollSetupActivity.java
@@ -1,25 +1,48 @@
 package casco.project1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class PollSetupActivity extends Activity implements AdapterView.OnItemClickListener{
-    ListView list;
+    TextView tvPollName;
+    TextView tvPollCreator;
+    TextView tvPollParticipants;
+    ListView lvParticipants;
+    Button btnRespond;
+    Button btnViewResults;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_poll_setup);
-        list = (ListView) findViewById(R.id.lv_participants);
+        Intent intent = getIntent();
+        String pollName = intent.getStringExtra("PollName");
+
+        tvPollName = (TextView) findViewById(R.id.tv_poll_name);
+
+        tvPollName.setText(pollName);
+        tvPollCreator = (TextView) findViewById(R.id.tv_poll_creator);
+        tvPollParticipants = (TextView) findViewById(R.id.tv_list_description);
+
+        btnRespond = (Button) findViewById(R.id.btn_respond);
+        btnViewResults = (Button) findViewById(R.id.btn_view_results);
+
+        lvParticipants = (ListView) findViewById(R.id.lv_participants);
         ArrayAdapter adapter = ArrayAdapter.createFromResource(
                 this,R.array.test_participants, android.R.layout.simple_list_item_1);
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(this);
+        lvParticipants.setAdapter(adapter);
+        lvParticipants.setOnItemClickListener(this);
     }
 
     @Override
